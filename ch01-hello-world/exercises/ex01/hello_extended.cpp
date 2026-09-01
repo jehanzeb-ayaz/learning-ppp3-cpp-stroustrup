aa742d384db8a73e541019c7afe5d9e245d9ae88
Jehanzeb Ayaz <jehanzeb1016@gmail.com>
2026-09-01 21:33:12 +0200

    Chapter 01. Exercise 01.

---
+++ ch01-hello-world/exercises/ex01/hello_extended.cpp
@@ -0,0 +1,11 @@
+// Chapter 01. Exercise 01.
+// A very simple program to output "Hello, programming!"
+// and on a new line "Here we go!" to the Terminal.
+
+#include <iostream>
+
+int main()      
+{
+    std::cout << "Hello, programming!\n" << "Here we go!\n";
+    return 0;
+}
