a0e3c8b89ee8d735a4d068000dd76ef1f0e9b39d
Jehanzeb Ayaz <jehanzeb1016@gmail.com>
2026-09-05 23:37:47 +0200

    Chapter 02. Try This 05.

---
+++ ch02-objects-types-values/try-this/tt05/goodbye_cruel_world.cpp
@@ -0,0 +1,14 @@
+/* Chapter 2. Try This 5. Remove the errors one by one, 
+ * starting with the lexically first, and see how the 
+ * error messages change (and improve).
+*/
+
+#include <iostream>
+
+int main()
+{
+        std::string s = "Goodbye, cruel world! ";
+        std::cout << s << '\n';
+
+        return 0;
+}
