dd095090b9a14ff796bd8b38130ed92486b1f51a
Jehanzeb Ayaz <jehanzeb1016@gmail.com>
2026-09-08 07:28:27 +0200

    Chapter 02. Drill 04. Used error() instead of simple_error().

--- ch02-objects-types-values/drills/letter.cpp
+++ ch02-objects-types-values/drills/letter.cpp
@@ -3,21 +3,28 @@
  * along with indentation.
 */
 
-#include <iostream>
+#include "../../support/PPPheaders.h"
 
 int main()      
 {
-         std::cout << "Enter the name of the person you want to write to:\n";
-         std::string first_name = "???";    // string variable ("???" indicates "don’t know the name")
-         std::cin >> first_name;     // read a string
+         std::cout << "Enter the name and the age of the person you want to write to:\n";
+         string first_name = "???";    // string variable ("???" indicates "don’t know the name")
+         int age = -1;                      // int variable (-1 means "don’t know the age")
+         std::cin >> first_name >> age;     // read a string
+         if (age < 0 || age > 110)
+         {
+            error("you're kidding!");
+         }
 
          std::cout << "Enter the name of the common friend:\n";
-         std::string friend_name = "???";    
+         string friend_name = "???";    
          std::cin >> friend_name;
 
          std::cout << "Dear " << first_name << ", \n";
          std::cout << "I hope you are doing well. I am fine here. I am \n";
          std::cout << "making steady progress here. How about you? How are\n";
-         std::cout << "things in San Francisco? And have you, by any chance,\n";
-         std::cout << "seen " << friend_name << " lately?\n";
+         std::cout << "things in San Francisco?\n";
+         std::cout << "I hear you just had a birthday and you are " << age << "\n";
+         std::cout << "years old. How was your birthday party? And have you,\n";
+         std::cout << "by any chance, seen " << friend_name << " lately?\n";
 }
