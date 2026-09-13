ced7907f7cf6e034d071a1c0315e538215680760
Jehanzeb Ayaz <jehanzeb1016@gmail.com>
2026-09-13 17:22:29 +0200

    Corrected logic. Chapter 02. Exercise 06.

--- ch02-objects-types-values/exercises/three_int_sequence.cpp
+++ ch02-objects-types-values/exercises/three_int_sequence.cpp
@@ -19,38 +19,31 @@ int main()
         int val3 = -1;
         cin >> val3;                       
         
-        if (val1 < val2)
+        if (val1 <= val2 && val2 <= val3)
         {
-            if (val2 <= val3)
-            {
-                cout << val1 << ", " << val2 << ", " << val3 << ".\n";
-            }
-            else if (val2 > val3)
-            {
-                cout << val1 << ", " << val3 << ", " << val2 << ".\n";
-            }
+            cout << val1 << ", " << val2 << ", " << val3 << ".\n";
         }
-        else if (val1 > val2)
+        else if (val1 <= val3 && val3 <= val2)
         {
-            if (val2 <= val3)
-            {
-                cout << val2 << ", " << val3 << ", " << val1 << ".\n";
-            }
-            else if (val2 > val3)
-            {
-                cout << val3 << ", " << val2 << ", " << val1 << ".\n";
-            }
+            cout << val1 << ", " << val2 << ", " << val2 << ".\n";
         }
-        else if (val1 == val2)
+
+        else if (val2 <= val1 && val1 <= val3)
         {
-            if (val2 <= val3)
-            {
-                cout << val1 << ", " << val2 << ", " << val3 << ".\n";
-            }
-            else if (val2 > val3)
-            {
-                cout << val3 << ", " << val2 << ", " << val1 << ".\n";
-            }
+            cout << val2 << ", " << val1 << ", " << val2 << ".\n";
+        }
+        else if (val2 <= val3 && val3 <= val1)
+        {
+            cout << val2 << ", " << val3 << ", " << val1 << ".\n";
+        }
+
+        else if (val3 <= val1 && val1 <= val2)
+        {
+            cout << val3 << ", " << val1 << ", " << val2 << ".\n";
+        }
+        else if (val3 <= val2 && val2 <= val1)
+        {
+            cout << val3 << ", " << val2 << ", " << val1 << ".\n";
         }
 
         return 0;
