227869a536d5622f35cde4a032cc48abc9549be0
Jehanzeb Ayaz <jehanzeb1016@gmail.com>
2026-09-18 11:54:01 +0200

    Switched variable and vector identifiers to improve readability in Chapter 03. Drill 07.

--- ch03-computation/drills/print_doubles_units.cpp
+++ ch03-computation/drills/print_doubles_units.cpp
@@ -11,17 +11,17 @@ int main()
     double val1= -1.0;
     vector<double> entered_numbers = {};
 
-    string unit = "";
-    vector<string> entered_units = {"cm", "m", "in", "ft"};
+    string entered_unit = "";
+    vector<string> units = {"cm", "m", "in", "ft"};
 
     cout << "Enter a floating-point value and a unit please ('|' to terminate):\n";
 
-    while (cin >> val1 >> unit)
+    while (cin >> val1 >> entered_unit)
     {
         entered_numbers.push_back(val1);
         ranges::sort(entered_numbers);
         
-        cout << val1 << unit << ".\n";
+        cout << val1 << entered_unit << ".\n";
 
         cout << "Enter two floating-point values please ('|' to terminate):\n";
     }
