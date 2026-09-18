df4a38b52257cbf833b96b32d8392101d2b5b3e2
Jehanzeb Ayaz <jehanzeb1016@gmail.com>
2026-09-18 15:42:16 +0200

    Chapter 03. Drill 09.

--- ch03-computation/drills/print_doubles_units.cpp
+++ ch03-computation/drills/print_doubles_units.cpp
@@ -1,4 +1,4 @@
-/* Chapter 3. Drill 8. A C++ program that asks the user
+/* Chapter 3. Drill 9. A C++ program that asks the user
  * to enter a double value and a unit (cm, m, in, ft)
  * (continuously in a while-loop) and print them, 
  * until the user  enters '|'.
@@ -8,6 +8,10 @@
 
 int main()      
 {
+    constexpr double m_to_cm = 100;          // 1 m = 100 cm
+    constexpr double in_to_cm = 2.54;        // 1 in = 2.54 cm
+    constexpr double ft_to_in = 12;          // 1 ft = 12 in
+
     double val1= -1.0;
     vector<double> entered_numbers = {};
 
@@ -16,11 +20,12 @@ int main()
 
     cout << "Enter a floating-point value and a unit please ('|' to terminate):\n";
 
+    double smallest = -1.0, largest = -1.0;
+    double sum = 0.0;           //in meters
+    double val_in_m = -1.0;
+
     while (cin >> val1 >> entered_unit)
     {
-        entered_numbers.push_back(val1);
-        ranges::sort(entered_numbers);
-        
         int flag = -1;
 
         for (string x : units)
@@ -38,9 +43,44 @@ int main()
             return 0;
         }
 
-        cout << val1 << entered_unit << ".\n";
+        if (entered_unit == "cm")
+        {
+            val_in_m = val1/m_to_cm;
+            sum = sum + val_in_m;
+        }
+        else if (entered_unit == "m")
+        {
+            val_in_m = val1;
+            sum = sum + val_in_m;
+        }
+        else if (entered_unit == "in")
+        {
+            double temp = -1.0;
+            temp = val1 * in_to_cm;
+            val_in_m = temp/m_to_cm;
+            sum = sum + val_in_m;
+        }
+        else if (entered_unit == "ft")
+        {
+            double temp1 = -1.0, temp2 = -1.0;
+            temp1 = val1 * ft_to_in;
+            temp2 = temp1 * in_to_cm;
+            val_in_m = temp2/m_to_cm;
+            sum = sum + val_in_m;
+        }
 
-        cout << "Enter two floating-point values please ('|' to terminate):\n";
+        entered_numbers.push_back(val_in_m);
+        ranges::sort(entered_numbers);
+
+        smallest = entered_numbers[0];
+        largest = entered_numbers[entered_numbers.size() - 1];
+
+        cout << smallest << " is the smallest so far.\n";
+        cout << largest << " is the largest so far.\n";
+        cout << "The number of values entered so far is " << entered_numbers.size() << ".\n";
+        cout << "The sum of values entered (in meters) is: " << sum << ".\n";
+
+        cout << "Enter a floating-point value and a unit please ('|' to terminate):\n";
     }
 
     return 0;
